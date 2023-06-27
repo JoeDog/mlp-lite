af06cefffd27f52af8951ec946f89079f36180cb
Jeff Fulmer <jeff@joedog.org>
2023-06-26 23:35:04 -0400

    Added XML annotations

--- src/org/joedog/ann/Bias.java
+++ src/org/joedog/ann/Bias.java
@@ -1,5 +1,6 @@
 package org.joedog.ann;
 
+import javax.xml.bind.annotation.XmlAttribute;
 
 public class Bias {
   private Neuron neuron; 
@@ -23,6 +24,14 @@ public class Bias {
     this.neuron.setBias(this);
   }
 
+  public Bias(Neuron neuron, double value, double weight, double momentum) {
+    this.neuron   = neuron;
+    this.value    = value;
+    this.weight   = weight;
+    this.momentum = 0.0;
+    this.neuron.setBias(this);
+  }
+
   public void remove() {
     this.neuron.setBias(null);
   }
@@ -37,14 +46,19 @@ public class Bias {
     return this.neuron;
   }
 
+  @XmlAttribute(name="value")
   public double getValue() {
     return this.value;
   }
 
+  @XmlAttribute(name="weight")
   public double getWeight() {
     return this.weight;
   }
   
+  public String asString() {
+    return "bv: "+this.value+", bw: "+this.weight+", bm: "+this.momentum;
+  }
   public String toString() {
     return "Bias(v: "+this.value+" w: "+this.weight+" m: "+this.momentum+")";
   }
