57b886aa62a85572a746952583c948ddf1b47fd3
Jeff Fulmer <jeff@joedog.org>
2023-12-21 13:25:32 -0500

    Updated with new Value

--- tests/Next.java
+++ tests/Next.java
@@ -1,35 +1,42 @@
 package tests;
 
+import java.util.Random;
 import org.joedog.ann.*;
+import org.joedog.ann.data.*;
 import org.joedog.ann.function.*;
+import org.joedog.util.io.FileUtils;
 
 public class Next {
-  
+  /**
+   * Given input n, produce the next consecutive number.
+   */
   public static void main(String [] args) {
-    MLP mlp = MLP.getInstance(new int[]{3,10,1}, new FunctionFactoryImpl().getFunction(Function.SIGMOIDAL), true);
+    MLP      mlp = null;
     Value [] val = new Value[20];
     for (int i = 1; i < val.length; i++) {
-      val[i-1] = new Value.ValueBuilder().inRange(0, 100, i);
+      val[i-1] = new Value.ValueBuilder().inRange(i, 1, 100);
     }
-    for (int i = 0; i < 16; i++) {
-      System.out.printf(
-        "%d, %d, %d => %d\n", 
-        val[i].asInt(), val[i+1].asInt(), val[i+2].asInt(), val[i+3].asInt()
-      );
-      mlp.addExample(new Value[]{val[i], val[i+1], val[i+2]}, new Value[]{val[i+3]});
+  
+    if (FileUtils.exists("next.xml")) {
+      mlp = MLP.getInstance("next.xml");
+      System.out.println("TRAINED :");
+    } else {
+      mlp = MLP.getInstance(new int[]{1,20,20,1}, new FunctionFactoryImpl().getFunction(Function.SIGMOIDAL), true);
+      for (int i = 0; i < val.length-2; i++) {
+        System.out.printf("Training sets: %d => %d\n", val[i].asInt(), val[i+1].asInt());
+        mlp.addExample(new Value[]{val[i]}, new Value[]{val[i+1]});
+      }
+      mlp.learnByExample(50000000);
+      mlp.save("next.xml");
+      System.out.println("LEARNED :");
     }
-    mlp.learnByExample(4000000);
-    System.out.println(mlp.toString());
-    System.out.println("LEARNED :");
-    System.out.printf( "  inputs: 3,  4,  5 output: %d\n", mlp.predict(
-      new Value[]{val[2], val[3], val[4]})[0].asInt()
-    );
-    System.out.printf( "  inputs: 7,  8,  9 output: %d\n", mlp.predict(
-      new Value[]{val[6], val[7], val[8]})[0].asInt()
-    );
-    System.out.printf( "  inputs: 9, 10, 11 output: %d\n", mlp.predict(
-      new Value[]{val[8], val[9], val[10]})[0].asInt()
-    );
+
+    for (int i = 0; i < 5; i++) {
+      Random   rdm = new Random();
+      int      r   = rdm.nextInt(19-1) + 1; 
+      Value [] in  = new Value[]{val[r]};
+      Value [] out = mlp.predict(in); 
+      System.out.printf( "  input: %d output: %d\n", in[0].asInt(), out[0].fromAnalog());
+    }
   }
 }
-
