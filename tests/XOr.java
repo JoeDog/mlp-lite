0e726f4656d16356ce111c622acceb96e4c6039c
Jeff Fulmer <jeff@joedog.org>
2023-12-21 13:24:34 -0500

    Updated with latest Value

--- tests/XOr.java
+++ tests/XOr.java
@@ -1,7 +1,9 @@
 package tests;
 
 import org.joedog.ann.*;
+import org.joedog.ann.data.*;
 import org.joedog.ann.function.*;
+import org.joedog.util.io.FileUtils;
 
 /**
  * @author Jeffrey Fulmer
@@ -20,21 +22,26 @@ public class XOr {
    * +---+---+---
    */
   public XOr() {
-    MLP mlp = MLP.getInstance(new int[]{2,2,2,2,1}, new FunctionFactoryImpl().getFunction(Function.SIGMOIDAL), true);  
+    MLP mlp      = null;
     Value trusie = new Value.ValueBuilder().asBoolean(true);
     Value falsie = new Value.ValueBuilder().asBoolean(false);
-    mlp.addExample(new Value[]{falsie, falsie}, new Value[]{falsie});
-    mlp.addExample(new Value[]{falsie, trusie}, new Value[]{trusie});
-    mlp.addExample(new Value[]{trusie, falsie}, new Value[]{trusie});
-    mlp.addExample(new Value[]{trusie, trusie}, new Value[]{falsie});
-    mlp.learnByExample(44000000);
-    //System.out.println(mlp.toString());
+
+    if (! FileUtils.exists("xor.xml")) {
+      mlp = MLP.getInstance(new int[]{2,2,2,2,1}, new FunctionFactoryImpl().getFunction(Function.SIGMOIDAL), true);  
+      mlp.addExample(new Value[]{falsie, falsie}, new Value[]{falsie});
+      mlp.addExample(new Value[]{falsie, trusie}, new Value[]{trusie});
+      mlp.addExample(new Value[]{trusie, falsie}, new Value[]{trusie});
+      mlp.addExample(new Value[]{trusie, trusie}, new Value[]{falsie});
+      mlp.learnByExample(40000000);
+      mlp.save("xor.xml");
+    } else {
+      mlp = MLP.getInstance("xor.xml");
+    }
     System.out.println("LEARNED :");
     System.out.printf( "  - 0 xor 0 = %s\n", mlp.predict(new Value[]{falsie, falsie})[0].asBoolean());
     System.out.printf( "  - 0 xor 1 = %s\n", mlp.predict(new Value[]{falsie, trusie})[0].asBoolean());
     System.out.printf( "  - 1 xor 0 = %s\n", mlp.predict(new Value[]{trusie, falsie})[0].asBoolean());
     System.out.printf( "  - 1 xor 1 = %s\n", mlp.predict(new Value[]{trusie, trusie})[0].asBoolean());
-    mlp.save("xor.xml");
   }
 
   public static void main(String[] args) {
